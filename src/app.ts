02f95f3018feababb3bbe65c1acb5d91ee00e5c8
DJ Drakos <dianajodrakos@gmail.com>
2021-05-13 13:37:32 -0700

    reset repo

--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import { it } from './utils';// import functions and grab DOM elements
-
-// initialize state
-const ok = it('fito');
-console.log(ok)
-
-// set event listeners to update state and DOM 
--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-type Dog = {
-    name: string
-}
-
-export function it(name) {
-    return {
-        name
-    }
-}
